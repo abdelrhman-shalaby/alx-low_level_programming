2f1534f06d426db38ec11b8583e81aa584c04974
abdelrhman.shalaby <abdelrhman.shalaby93@gmail.com>
2023-02-16 16:23:48 -0800

    auto commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf("Size of a char:%lu byte(s)", size of(char));
-printf("Size of an int:%lu byte(s)", size of(int));
-printf("Size of a long int:%lu byte(s)", size of(long int));
-printf("Size of a long long int:%lu byte(s)", size of(long long int));
-printf("Size of a float:%lu byte(s)", size of(float));
+printf("Size of a char:%zubyte(s)", sizeof(char));
+printf("Size of an int:%zubyte(s)", sizeof(int));
+printf("Size of a long int:%zubyte(s)", sizeof(long int));
+printf("Size of a long long int:%zubyte(s)", sizeof(long long int));
+printf("Size of a float:%zubyte(s)", sizeof(float));
 return (0);
 }
-~  
