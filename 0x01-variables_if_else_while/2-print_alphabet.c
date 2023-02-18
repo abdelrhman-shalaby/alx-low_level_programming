4ede1d60cab232bf97c616ea097e6158fca9c01c
abdelrhman.shalaby <abdelrhman.shalaby93@gmail.com>
2023-02-18 07:19:19 -0800

    auto commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - small every litter
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0, i < 27, i++)
+	{
+		putchar(a[i]);
+	}
+	putchar('\n');
+	return (0);
+}
