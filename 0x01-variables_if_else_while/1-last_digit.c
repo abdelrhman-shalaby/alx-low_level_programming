0736979f177a5affe9b00b867b3a50d4301f70de
abdelrhman.shalaby <abdelrhman.shalaby93@gmail.com>
2023-02-18 02:57:51 -0800

    auto commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l_digit);
 	}
-	else if	(l_digit < 5)
+	else if	(l_digit < 5 && l_digit != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l_digit);
 	}
