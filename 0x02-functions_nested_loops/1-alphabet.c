7004d3090385ab0613389ffb1ab3d1710a497a79
abdelrhman.shalaby <abdelrhman.shalaby93@gmail.com>
2023-02-23 14:22:37 -0800

    auto commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,12 @@
 /**
  * print_alphabet - print all alphabet in lowercase
  * return:0
- */
+*/
 void print_alphabet(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 	_putchar('\n');
 }
